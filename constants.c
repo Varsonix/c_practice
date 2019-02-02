e34d2eeacec6272a4c2f2738ba7005745703fab7
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2019-02-02 01:31:36 -0800

    Create constants.c

---
+++ constants.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Checks user input and prints a message depending on whether the number input is even or odd.
+int main()
+{
+    /* Constants */
+    /* These cannot be modified, or else an error will be thrown */
+    const int NUM = 5;
+    const char myChar = 'a';
+    const char myString[] = "My string!";
+    printf("%d\n", NUM);
+    printf("%d\n", NUM);
+    printf("%c\n", myChar);
+    printf("%s", myString);
+    
+    return 0;
+}

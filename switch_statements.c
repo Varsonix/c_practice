835abec309f4b6e4d06d83fde35a654a999132a7
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2019-02-05 14:37:32 -0800

    Create switch_statements.c

---
+++ switch_statements.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Lesson on switch cases, similar to if/else statements.
+int main()
+{
+    // Declaring grade variable
+    char grade;
+
+    // Having the user input the grade (To test the case)
+    printf("Please input your grade: ");
+    scanf(" %c", &grade);
+    switch(grade){
+    case 'A' :
+        printf("You did great!");
+        break;
+    case 'B' :
+        printf("You did alright!");
+        break;
+    case 'C' :
+        printf("You did poorly.");
+        break;
+    case 'D' :
+        printf("You did very bad.");
+        break;
+    case 'F' :
+        printf("You failed!");
+        break;
+    case 'E':
+        printf("What are you even doing?!");
+        break;
+    default :
+        printf("Invalid Grade.");
+        break;
+
+    }
+
+    return 0;
+}
